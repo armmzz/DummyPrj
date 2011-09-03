7164129418ab821dfec7353911e9b044fd1c641e
Korpong Wongpatikaseree <a_m867@hotmail.com>
2011-09-03 10:24:48 +0700

    First Commit
    Window Based Template

---
+++ DummyPrj/DummyPrjAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DummyPrjAppDelegate.h
+//  DummyPrj
+//
+//  Created by Korpong Wongpatikaseree on 9/3/54 BE.
+//  Copyright 2554 TNS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DummyPrjAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
